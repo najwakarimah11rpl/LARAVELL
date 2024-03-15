83699575bb5efdcdd5e2c658f35f48b89cc951d8
NajwaKarimahN <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-03-15 10:35:03 +0700

    Update kategori.php

--- controller/kategori.php
+++ controller/kategori.php
@@ -1,22 +1,12 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Model\Menu;
 use Illuminate\Http\Request;
-
-class MenuController extends Controller
-{
+class MenuController extends Controller{
     /**
      *Display a listing of the resource.
      * @return\Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
-        echo "<h1>ini index kategori</h1>"        
-    }
-}
-
-
+public function index(){
+    echo "<h1>ini index kategori</h1>"}}
 ?>
