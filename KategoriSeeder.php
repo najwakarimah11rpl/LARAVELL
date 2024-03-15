6e4aeb93fbfb0b27514e4e635f4bd2c4df988316
NajwaKarimahN <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-03-15 09:11:23 +0700

    Update KategoriSeeder.php

--- KategoriSeeder.php
+++ KategoriSeeder.php
@@ -1,19 +1,12 @@
 <?php
-
 namespace Database\Seeders;
-
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 class KategoriSeeder extends Seeder
-{
-    /**
+{  /**
      * Run the database seeds;
      *
      * @return void
      */
-    public function run()
-    {
-        //
-    }
+ public function run() {//}
 }
