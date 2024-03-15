a38a8811cb1c9f014440a5191ccd19b622274436
NajwaKarimahN <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-03-15 09:25:56 +0700

    Update OrderSeeder.php

--- OrderSeeder.php
+++ OrderSeeder.php
@@ -1,24 +1,16 @@
 <?php
-
 namespace Database\Seeders;
-
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
-class OrderSeeder extends Seeder
-{
+class OrderSeeder extends Seeder{
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
-}
+public function run(){
+    // \App\Models\User::factory(10)->create();
+    // \App\Models\User::factory()->create([
+    //     'name' => 'Test User',
+    //     'email' => 'test@example.com',
+    // ]);}}
