8f0fc773b942654d60bd5d698c64fd4a1eea882d
NajwaKarimahN <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-03-15 09:24:42 +0700

    Update MenuSeeder.php

--- MenuSeeder.php
+++ MenuSeeder.php
@@ -1,19 +1,11 @@
 <?php
-
 namespace Database\Seeders;
-
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
-class MenuSeeder extends Seeder
-{
+class MenuSeeder extends Seeder{
     /**
      * Run the database seeds;
      *
      * @return void
      */
-    public function run()
-    {
-        //
-    }
-}
+public function run(){//}}
