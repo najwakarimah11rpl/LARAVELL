c23be52ce477ed3b51c8ccaf4438fe02677ae47f
NajwaKarimahN <147923660+najwakarimah11rpl@users.noreply.github.com>
2024-03-15 10:36:29 +0700

    Update menu.php

--- controller/menu.php
+++ controller/menu.php
@@ -1,27 +1,15 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Model\Menu;
 use Illuminate\Http\Request;
-
-class MenuController extends Controller
-{
+class MenuController extends Controller{
     /**
      *Display a listing of the resource.
      * @return\Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
-        echo "<h1>ini index menu</h1>"        
-    }
-    
-    @param \App\Models\Menu $menu
-    @return \Illuminate\Http\Response
-    
-    public function edit($id);
-}
-
-
+public function index(){
+    echo "<h1>ini index menu</h1>"}
+@param \App\Models\Menu $menu
+@return \Illuminate\Http\Response
+public function edit($id);}
 ?>
